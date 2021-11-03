e200cde6c4e840783ecbc8c95202e9bc2833d3b7
NaRyung Kim <criticalpxint@naver.com>
2021-11-03 19:47:50 +0900

    포스트 목록/상세 페이지에 태그 기능 추가 및 태그페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import User
 import os
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}'
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -28,8 +39,10 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User,null=True,on_delete=models.SET_NULL)
+
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
-    #author
+
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 #def index(request):
 class PostList(ListView):
@@ -44,6 +44,20 @@ def category_page(request, slug):
                       'category' : category,
                   }
                   )
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list =tag.post_set.all() #Post.objects.filter(tags=tag)
+
+    return render(request, 'blog/post_list.html',
+                  {
+                      'post_list' : post_list,
+                      'categories' : Category.objects.all(),
+                      'no_category_post_count' : Post.objects.filter(category=None).count(),
+                      'tag': tag,
+                  }
+                  )
+
 
 #        posts = Post.objects.all().order_by('-pk')
 
