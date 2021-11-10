1adf8038019c2fc8eb2025be5cfb0e4a15a828d7
NaRyung Kim <criticalpxint@naver.com>
2021-11-10 16:18:16 +0900

    포스트 작성/수정 태그 지정하기

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,12 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Category, Tag
 from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 
 #def index(request):
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
@@ -16,13 +17,26 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         current_user = self.request.user
         if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author = current_user
-            return super(PostCreate, self).form_valid(form)
+            response = super(PostCreate, self).form_valid(form)
+            tags_str = self.request.POST.get('tags_str')
+            if tags_str:
+                tags_str = tags_str.strip()
+                tags_str = tags_str.replace(',',';')
+                tags_list = tags_str.split(';')
+                for t in tags_list:
+                    t = t.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+            return response
         else :
             return redirect('/blog/')
 
 class PostUpdate(LoginRequiredMixin, UpdateView): #모델명_form
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
     template_name = 'blog/post_update_form.html'
 
@@ -31,11 +45,39 @@ class PostUpdate(LoginRequiredMixin, UpdateView): #모델명_form
             return super(PostUpdate,self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exist():
+            tags_str_list = list()
+            for t in self.object.tags.all():
+                tags_str_list.append(t.name)
+            context['tag_str_default'] = ';'.join(tags_str_list)
+        return context
+
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str:
+            tags_str = tags_str.strip()
+            tags_str = tags_str.replace(',', ';')
+            tags_list = tags_str.split(';')
+            for t in tags_list:
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                if is_tag_created:
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+        return response
+
+
 class PostList(ListView):
     model = Post
     ordering = '-pk'
-#    template_name = 'blog/post_list.html'
-# post_list.html
+    #    template_name = 'blog/post_list.html'
+    # post_list.html
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostList, self).get_context_data()
@@ -95,12 +137,12 @@ def tag_page(request, slug):
 #                     )
 
 #def single_post_page(request, pk):
- #   post = Post.objects.get(pk=pk)
+#   post = Post.objects.get(pk=pk)
 
-  #  return render(request, 'blog/post_detail.html',
-   #               {
-    #                  'post': post
-     #             }
-      #            )
+#  return render(request, 'blog/post_detail.html',
+#               {
+#                  'post': post
+#             }
+#            )
 
 # Create your views here.
