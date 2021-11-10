40a35f4dce995287311ee3f0b84df95f22d767e0
NaRyung Kim <criticalpxint@naver.com>
2021-11-10 16:52:05 +0900

    crispy-forms와 markdownx 라이브러리 이용하기

--- blog/views.py
+++ blog/views.py
@@ -48,21 +48,24 @@ class PostUpdate(LoginRequiredMixin, UpdateView): #모델명_form
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostUpdate, self).get_context_data()
-        if self.object.tags.exist():
+        if self.object.tags.exists():
             tags_str_list = list()
             for t in self.object.tags.all():
                 tags_str_list.append(t.name)
-            context['tag_str_default'] = ';'.join(tags_str_list)
+            context['tags_str_default'] = ';'.join(tags_str_list)
+
         return context
 
     def form_valid(self, form):
         response = super(PostUpdate, self).form_valid(form)
         self.object.tags.clear()
+
         tags_str = self.request.POST.get('tags_str')
         if tags_str:
             tags_str = tags_str.strip()
             tags_str = tags_str.replace(',', ';')
             tags_list = tags_str.split(';')
+
             for t in tags_list:
                 t = t.strip()
                 tag, is_tag_created = Tag.objects.get_or_create(name=t)
@@ -70,6 +73,7 @@ class PostUpdate(LoginRequiredMixin, UpdateView): #모델명_form
                     tag.slug = slugify(t, allow_unicode=True)
                     tag.save()
                 self.object.tags.add(tag)
+
         return response
 
 
--- myInternetPrj/settings.py
+++ myInternetPrj/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'markdownx',
     'blog',
     'single_pages'
 ]
@@ -122,10 +124,13 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
