7091493b98cbe3cfa3fb50a6db467f6df7580dc4
NaRyung Kim <criticalpxint@naver.com>
2021-11-10 12:33:17 +0900

    포스트 수정 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [ #   서버IP/blog/
     #path('<int:pk>/',views.single_post_page),
     #path('', views.index),
 
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/',views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page), #서버IP/blog/category/slug
