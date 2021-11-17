523424b7cd086b57929e4c2e3b7338d6f84c130f
NaRyung Kim <criticalpxint@naver.com>
2021-11-17 19:36:32 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 # Create your tests here.
 class TestView(TestCase):
@@ -19,6 +19,7 @@ class TestView(TestCase):
         self.tag_python_kor = Tag.objects.create(name='파이썬 공부', slug='파이썬-공부')
         self.tag_python = Tag.objects.create(name='python', slug='python')
         self.tag_hello = Tag.objects.create(name='hello', slug='hello')
+
         # 포스트(게시물)이 3개 존재하는 경우
         self.post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
@@ -35,12 +36,18 @@ class TestView(TestCase):
         )
 
         self.post_003 = Post.objects.create(
-            title='Post form 만들기',
-            content='Post form 만들기',
+            title='세 번째 포스트입니다.',
+            content='세번째 포스틥니다.',
             author=self.user_felix,
         )
         self.post_003.tags.add(self.tag_python)
         self.post_003.tags.add(self.tag_python_kor)
+
+        self.comment_001 = Comment.objects.create(
+            post = self.post_001,
+            author = self.user_felix,
+            content = '첫번째 댓글입니다',
+        )
 
     def navbar_test(self,soup):
         # 네비게이션바가 있는가
@@ -114,7 +121,7 @@ class TestView(TestCase):
 
         # 페이지 타이틀 'Blog'
         soup = BeautifulSoup(response.content, 'html.parser')
-        self.assertEqual(soup.title.text, 'Created Post - Blog')
+        self.assertEqual('Created Post - Blog', soup.title.text)
         main_area = soup.find('div', id='main-area')
         self.assertIn('Create New Post',main_area.text)
 
@@ -123,14 +130,13 @@ class TestView(TestCase):
 
         self.client.post('blog/create_post',
             {
-                'title' : 'Post form 만들기',
-                'content' : "post form 페이지 만들기",
-                'tags_str' : 'new tag; 한글태그, python'
+                'title': 'Post form 만들기',
+                'content': "Post form 페이지 만들기",
+                'tags_str': 'new tag; 한글태그, python'
             })
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post form 만들기")
-        self.assertEqual(last_post.author.username,'Felix')
-
+        self.assertEqual(last_post.author.username,'felix')
         self.assertEqual(last_post.tags.count(),3)
         self.assertTrue(Tag.objects.get(name='new tag'))
         self.assertTrue(Tag.objects.get(name='한글태그'))
@@ -139,17 +145,17 @@ class TestView(TestCase):
     def test_update_post(self):
         update_url = f'/blog/update_post/{self.post_003.pk}/'
         # 로그인하지 않은 경우
-        response = self.client.get('/blog/update_post/{self.post_003.pk}')
+        response = self.client.get(update_url)
         self.assertNotEqual(response.status_code, 200)
 
         # 로그인 했지만 작성자가 아닌 경우
         self.assertNotEqual(self.post_003.author, self.user_james)
-        self.client.login(username='James', password='somepassword')
+        self.client.login(username='james', password='somepassword')
         response = self.client.get(update_url)
         self.assertEqual(response.status_code,403) #403:forbidden (접근 권한 금지)
 
         # 작성자가 로그인해서 접근한 경우
-        self.client.login(username='Felix', password='somepassword')
+        self.client.login(username='felix', password='somepassword')
         response = self.client.get(update_url)
         self.assertEqual(response.status_code, 200)
 
@@ -180,11 +186,8 @@ class TestView(TestCase):
         self.assertIn('some tag', main_area.text)
         self.assertNotIn('python', main_area.text)
 
-
-
     def test_post_list(self):
         self.assertEqual(Post.objects.count(),3) #########
-
         # 포스트 목록 페이지를 가져온다
         response = self.client.get('/blog/')
         # 정상적으로 페이지가 로드
@@ -259,10 +262,14 @@ class TestView(TestCase):
         self.assertIn(self.tag_hello.name, post_area.text)
         self.assertNotIn(self.tag_python.name, post_area.text)
         self.assertNotIn(self.tag_python_kor.name, post_area.text)
-
         # 포스터 작성자가 있는가
         # 아직 작성중
         # 포스트의 내용이 있는가
         self.assertIn(self.post_001.content, post_area.text)
 
         self.assertIn(self.user_james.username.upper(), post_area.text)
+
+        comments_area = soup.find('div', id='comment-area')
+        comment_001_area = comments_area.find('div', id='comment-1')
+        self.assertIn(self.comment_001.author.username,comment_001_area.text)
+        self.assertIn(self.comment_001.content, comment_001_area.text)
